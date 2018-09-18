d0cb91feac32e62fc1b0bcaa04b01dbdc5909bb3
richardcbrown <richardcharlesbrown88@gmail.com>
2018-09-18 19:51:01 +0100

    two item get price test fail

--- Checkout.Tests/CheckoutTests.cs
+++ Checkout.Tests/CheckoutTests.cs
@@ -17,5 +17,18 @@ namespace Checkout.Tests
 
             Assert.AreEqual(50, price);
         }
+
+        [Test]
+        public void GetTotalPrice_TwoItemsAdded_ReturnsCombinedPrice()
+        {
+            ICheckout checkout = new KataCheckout();
+
+            checkout.Scan("A");
+            checkout.Scan("B");
+
+            int price = checkout.GetTotalPrice();
+
+            Assert.AreEqual(80, price);
+        }
     }
 }
